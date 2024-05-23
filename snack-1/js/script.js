eb6d85e86615efff2140b7016751ae2f0c94ff1d
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-23 16:29:51 +0200

    Snack1: creatp l'array con il nome dei vips

---
+++ snack-1/js/script.js
@@ -0,0 +1,17 @@
+//SNACK 1:
+
+const nomeTavolo = 'Tavolo Vip';
+
+const vips =[
+    'Brad Pitt', 
+    'Johnny Depp', 
+    'Lady Gaga', 
+    'Cristiano Ronaldo', 
+    'Georgina Rodriguez', 
+    'Chiara Ferragni',  
+    'George Clooney', 
+    'Amal Clooney', 
+    'Fedez', 
+    'Amadeus', 
+    'Fiorello'
+];
