fee21f6316bcdf024be70888c4dab8ec656d7975
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-23 16:43:48 +0200

    Snack2: creato un array con all'interno i dati degli studenti

---
+++ snack-2/js/script.js
@@ -0,0 +1,46 @@
+//SNACK 2:
+
+const studentiFacoltà = [
+    {
+        id: 213,
+        Name: 'Giuseppina della Rovere' ,
+        Grades: 78,
+    },
+
+    {
+        id: 110,
+        Name: 'Paola Cortellesa',
+        Grades: 96,
+    },
+
+    {
+        id: 250,
+        Name: 'Andrea Mantegna',
+        Grades: 48,
+    },
+
+    {
+        id: 145,
+        Name: 'Gaia Borromini',
+        Grades:74,
+    },
+
+    {
+        id: 196,
+        Name:'Luigi Grimaldell',
+        Grades: 68,
+    },
+
+    {
+        id: 102,
+        Name: 'Piero della Francesca',
+        Grades: 50,
+    },
+
+    {
+        id: 120,
+        Name: 'Francesca da Polenta ',
+        Grades: 84,
+    },
+];
+
