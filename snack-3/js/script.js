684fd31daf5d0b0f6956ba73e8d347be19c5418d
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-25 09:03:04 +0200

    creato array con bici e relative caratteristiche

---
+++ snack-3/js/script.js
@@ -0,0 +1,9 @@
+const bici = [
+    { nome: 'Bianchi Oltre XR4', peso: 7.2 },
+    { nome: 'Pinarello Dogma F12', peso: 6.8 },
+    { nome: 'Specialized S-Works Tarmac', peso: 7.0 },
+    { nome: 'Canyon Ultimate CF SLX', peso: 6.7 },
+    { nome: 'Trek Emonda SLR', peso: 6.5 }
+
+];
+
